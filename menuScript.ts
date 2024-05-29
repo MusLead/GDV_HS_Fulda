abf72cd6f64f1f255d06e4ce101e81e87cc770c6
Agha Muhammad Aslam <aghaaslam246@gmail.com>
2024-05-29 15:39:07 +0200

    docs: add detail (sidebar menu automation)

--- menuScript.ts
+++ menuScript.ts
@@ -2,9 +2,7 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const header = document.querySelector('#header h3');
     const queryParams = new URLSearchParams(window.location.search);
-    const scriptName = queryParams.get('script') || "/src/cg/main";  // Default to 'main'
-
-    let scriptPath = scriptName;  // Assuming a direct map, for simplicity
+    const scriptPath = queryParams.get('script') || "/src/cg/main";  // Default to 'main'
 
     // console.log(`Loading script: ${scriptPath}`);
     updateHeader(scriptPath);
@@ -17,7 +15,9 @@ document.addEventListener('DOMContentLoaded', async () => {
         const parts = scriptName.split('/');
         const lastPart = parts[parts.length - 1];
         // Format the last part to replace underscores and camel case with spaces and proper capitalization
-        const displayName = lastPart.replace(/_/g, ' ').replace(/([a-z])([A-Z])/g, '$1 $2').replace(/\b\w/g, l => l.toUpperCase());
+        const displayName = lastPart.replace(/_/g, ' ')
+                                .replace(/([a-z])([A-Z])/g, '$1 $2')
+                                .replace(/\b\w/g, l => l.toUpperCase());
         // Update header with the formatted display name
         if (header) {
             header.textContent = displayName;
