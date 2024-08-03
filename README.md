8fc8c25f1f0d0bb2cb93631bdd1b8fc40429b825
Agha Muhammad Aslam <aghaaslam246@gmail.com>
2024-08-03 08:37:20 +0200

    fix: some errors

--- README.md
+++ README.md
@@ -3,16 +3,18 @@
 Name: Agha Muhammad Aslam
 
 Matrikelnr.: 1515659
- 
-### install dependencies
-```
+
+## install dependencies
+
+```sh
 npm install
 ```
 
 First time cloning this repos, you will need to install the Dependecies first.
 Also if there are new dependencies added tot he project (you could see them in package.json), you should npm install.
 
-### Run the application 
-```
+## Run the application
+
+```sh
 npm run dev
 ```
--- src/cg/Ray_Tracing/specularAndViewingAnglePG.ts
+++ src/cg/Ray_Tracing/specularAndViewingAnglePG.ts
@@ -10,22 +10,23 @@ const v = [1,1,1] // viewing vector (y -> -.5)
 pg.gridXZ();
 pg.visVector(v, { color: "blue", label: "V" });
 pg.visVector(n, { color: "dodgerblue", label: "N" });
-pg.visVector(i, { color: "orangered",  placeAt: vecMultiplyScalar(-1,i)});
+pg.visVector(i, { color: "orangered",  placeAt: vecMultiplyScalar(-1, i as Vec3), label: "I" });
 
 // relfection
 const reflected =
     vecSubtract(
-        vecNormalize(i),
+        vecNormalize(i as Vec3),
         vecMultiplyScalar(
-            vecDotProduct(vecNormalize(n as Vec3, vecNormalize(i as Vec3)),
-            vecMultiplyScalar(2, vecNormalize(n)),
+            vecDotProduct(vecNormalize(n as Vec3), vecNormalize(i as Vec3)),
+            vecMultiplyScalar(2, vecNormalize(n as Vec3) as Vec3)
         )
     )
+
 pg.visVector(reflected, { color: "orange", label: "reflected" });
 
 
 
 
 // Visualize view angle and specular
-const cosViewAngle = vecDotProduct(vecNormalize(reflected),vecNormalize(v));
-pg.visVector(vecMultiplyScalar(cosViewAngle, vecNormalize(v)),{color:"red",label:"specular"})
+const cosViewAngle = vecDotProduct(vecNormalize(reflected),vecNormalize(v as Vec3));
+pg.visVector(vecMultiplyScalar(cosViewAngle, vecNormalize(v as Vec3)),{color:"red",label:"specular"})
